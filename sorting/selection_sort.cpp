413bb76556617fe32f5e0d28a1842c65f7d4647a
beemarsh <anup8eguy@gmail.com>
2021-01-12 12:13:13 +0545

    Update sorting code

--- sorting/selection_sort.cpp
+++ sorting/selection_sort.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
 int main(){
+    int n;
+    std::cin >> n;
 
+    int a[n];
+    for(int i=0;i<n;i++){
+        std::cin >> a[i];
+    }
+
+    for(int i=0;i<n-1;i++){
+        int minIdx = i;
+        for(int j=i+1;j<n;j++){
+            if(a[j] < a[minIdx]){
+                minIdx = j;
+            }
+        }
+        
+        int temp = a[i];
+        a[i] = a[minIdx];
+        a[minIdx] = temp;
+    }
+    
+    for(int i=0;i<n;i++){
+        std::cout << a[i] << " ";
+    }
+
+    return 0;
 }
