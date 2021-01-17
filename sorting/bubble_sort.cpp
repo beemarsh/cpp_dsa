4af5a2aa77dd0c53e035a0597bacb564707f39f8
beemarsh <anup8eguy@gmail.com>
2021-01-17 12:13:13 +0545

    Bubble Sort Code

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -8,5 +8,21 @@ int main(){
 		std::cin >> a[i];
 	}
 	
+	int counter = n-1;
+
+	while(counter > 0){
+		for(int i=0;i<counter;i++){
+			if(a[i] > a[i+1]) {
+				int temp = a[i];
+				a[i] = a[i+1];
+				a[i+1] = temp;
+				}
+		}
+		counter--;
+	}
+
+	for(int i=0;i<n;i++){
+		std::cout << a[i] << " ";
+	}
 	return 1;
 }
