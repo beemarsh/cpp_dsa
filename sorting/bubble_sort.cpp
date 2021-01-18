437aaa7e9d6000e3021d68f6bb2db395c4cf607a
beemarsh <anup8eguy@gmail.com>
2021-01-18 12:13:13 +0545

    Bubble Sort Code Formatted

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 
-int main(){
-	int n;
-	std::cin >> n;
-	int a[n];
-	for(int i=0;i<n;i++){
-		std::cin >> a[i];
-	}
-	
-	int counter = n-1;
+int main() {
+  int n;
+  std::cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    std::cin >> a[i];
+  }
 
-	while(counter > 0){
-		for(int i=0;i<counter;i++){
-			if(a[i] > a[i+1]) {
-				int temp = a[i];
-				a[i] = a[i+1];
-				a[i+1] = temp;
-				}
-		}
-		counter--;
-	}
+  int counter = n - 1;
 
-	for(int i=0;i<n;i++){
-		std::cout << a[i] << " ";
-	}
-	return 1;
+  while (counter > 0) {
+    for (int i = 0; i < counter; i++) {
+      if (a[i] > a[i + 1]) {
+        int temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
+      }
+    }
+    counter--;
+  }
+
+  for (int i = 0; i < n; i++) {
+    std::cout << a[i] << " ";
+  }
+  return 1;
 }
