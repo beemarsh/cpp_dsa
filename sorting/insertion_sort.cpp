99d782545e33e98d67c18c381f5c57f3f588bdd0
beemarsh <anup8eguy@gmail.com>
2021-01-24 12:13:13 +0545

    Insertion Sort Code

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
 int main(){
+	int n;
+	std::cin >> n;
 
+	int a[n];
+	for(int i=0;i<n;i++){
+		std::cin >> a[i];
+	}
+
+	for(int i=1;i<n;i++){
+		int curr = a[i];
+		int j = i-1;
+		while(a[j] > curr && j >=0){
+			a[j+1] = a[j];	
+			j--;
+		}
+		a[j+1]=curr;
+	}
+
+	for(int i=0;i<n;i++){
+		std::cout << a[i] << " ";
+	}
 	return 1;
 }
