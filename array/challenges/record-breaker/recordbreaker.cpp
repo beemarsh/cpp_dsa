42a07d0c38e05190c61e82e35dbf173e5f1808b1
beemarsh <anup8eguy@gmail.com>
2021-02-02 12:13:13 +0545

    Record Breaking Challenge Code

---
+++ array/challenges/record-breaker/recordbreaker.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <climits>
+
+int main(){
+	
+	int n;
+	std::cin >> n;
+
+	int a[n+1];
+	for(int i=0;i<n;i++){
+		std::cin>> a[i];
+	}
+
+	a[n] = INT_MIN;
+	
+	int max = INT_MIN;
+	int rb_days = 0;
+
+	for(int i=0;i<n;i++){
+		if(a[i] > max && a[i] > a[i+1]){
+			rb_days++;
+		}
+		max = std::max(max,a[i]);
+	}
+
+	std::cout << rb_days;
+}
