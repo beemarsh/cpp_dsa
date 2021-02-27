e575748ff13e1de0431b5e9107286d95aae1b8ad
beemarsh <anup8eguy@gmail.com>
2021-02-27 12:13:13 +0545

    Sub array code

---
+++ subarray/challenges/subarray_with_given_sum/subarray_with_given_sum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main(){
+	int n;
+	std::cin >> n;
+
+	int S;
+	std::cin >> S;
+
+	int a[n];
+	for(int i=0;i<n;i++){
+		std::cin>>a[i];
+	}
+
+	int rp=0,lp=0;
+	int currSum = 0;
+
+	for(int i=0;i<n;i++){
+		if(a[rp] + currSum > S){
+			currSum -= a[lp];
+			lp++;
+		}
+		else if(a[rp] + currSum < S){
+			currSum+=a[rp];
+			rp++;
+		}
+		else break;
+	}	
+
+	std::cout << lp + 1 << " " << rp + 1;
+}
