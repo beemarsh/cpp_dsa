b80af5ad2e737cea0e32bb663d2b927c0d84618b
beemarsh <anup8eguy@gmail.com>
2021-03-11 12:13:13 +0545

    First repeating element code format

--- subarray/challenges/first_repeating_element/first_repeating_element.cpp
+++ subarray/challenges/first_repeating_element/first_repeating_element.cpp
@@ -1,32 +1,31 @@
-#include <iostream>
 #include <climits>
+#include <iostream>
 
-int main(){
-	int n;
-	std::cin >> n;
+int main() {
+  int n;
+  std::cin >> n;
 
-	int a[n];
-	for(int i=0;i<n;i++){
-		std::cin >> a[i];
-	}
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    std::cin >> a[i];
+  }
 
-	int N = 1e6 + 2;
-	int checkA[N];
+  int N = 1e6 + 2;
+  int checkA[N];
 
-	for(int i=0;i<N;i++){
-		checkA[i] = -1;
-	}
-	
-	int minIdx=INT_MAX;
+  for (int i = 0; i < N; i++) {
+    checkA[i] = -1;
+  }
 
-	for(int i=0;i<n;i++){
-		if(checkA[a[i]] == -1){
-			checkA[a[i]] = i;
-		}
-		else{
-			minIdx = std::min(minIdx,checkA[a[i]]);
-		}
-	}
+  int minIdx = INT_MAX;
 
-	std::cout << minIdx;
+  for (int i = 0; i < n; i++) {
+    if (checkA[a[i]] == -1) {
+      checkA[a[i]] = i;
+    } else {
+      minIdx = std::min(minIdx, checkA[a[i]]);
+    }
+  }
+
+  std::cout << minIdx;
 }
