f4d0436408d41a918bc241aa1ec406b4a470f215
beemarsh <anup8eguy@gmail.com>
2021-03-29 12:13:13 +0545

    Smallest Postiive Missing Number Code

--- array/challenges/smallest_positive_missing_number/smallest_positive_missing_number.cpp
+++ array/challenges/smallest_positive_missing_number/smallest_positive_missing_number.cpp
@@ -9,11 +9,23 @@ int main(){
 		std::cin >> a[i];
 	}
 
-	int N = 1e6 + 2;
-	int checkA[N];
+	int N = 1e6 +2;
+	bool checkA[N];
+	
+	for(int i=0;i<N;i++){
+		checkA[i] = 0;
+	}
+
 
 	for(int i=0;i<n;i++){
 		if(a[i] >=0) checkA[a[i]] = 1;
+	}
+
+	for(int i=0;i<N;i++){
+		if(checkA[i] == 0){
+			std::cout << i;
+			break;
+		}
 	}
 
 
