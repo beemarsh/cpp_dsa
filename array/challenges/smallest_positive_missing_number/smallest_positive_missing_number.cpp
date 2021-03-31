d5a623b3869ced833dd9b938ee83fd5f0a7611dd
beemarsh <anup8eguy@gmail.com>
2021-03-31 12:13:13 +0545

    Smallest Postiive Missing Number Code Formatted

--- array/challenges/smallest_positive_missing_number/smallest_positive_missing_number.cpp
+++ array/challenges/smallest_positive_missing_number/smallest_positive_missing_number.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
 
-int main(){
-	int n;
-	std::cin >> n;
+int main() {
+  int n;
+  std::cin >> n;
 
-	int a[n];
-	for(int i=0;i<n;i++){
-		std::cin >> a[i];
-	}
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    std::cin >> a[i];
+  }
 
-	int N = 1e6 +2;
-	bool checkA[N];
-	
-	for(int i=0;i<N;i++){
-		checkA[i] = 0;
-	}
+  int N = 1e6 + 2;
+  bool checkA[N];
 
+  for (int i = 0; i < N; i++) {
+    checkA[i] = 0;
+  }
 
-	for(int i=0;i<n;i++){
-		if(a[i] >=0) checkA[a[i]] = 1;
-	}
+  for (int i = 0; i < n; i++) {
+    if (a[i] >= 0) checkA[a[i]] = 1;
+  }
 
-	for(int i=0;i<N;i++){
-		if(checkA[i] == 0){
-			std::cout << i;
-			break;
-		}
-	}
-
-
-
+  for (int i = 0; i < N; i++) {
+    if (checkA[i] == 0) {
+      std::cout << i;
+      break;
+    }
+  }
 }
