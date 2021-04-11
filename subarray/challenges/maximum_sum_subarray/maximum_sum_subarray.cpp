dbb3511e8e17f5420339890793cb15a2d005fcbd
beemarsh <anup8eguy@gmail.com>
2021-04-11 12:13:13 +0545

    Maximum Sum Subarray Code

---
+++ subarray/challenges/maximum_sum_subarray/maximum_sum_subarray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+
+int main(){
+	int n;
+	std::cin >> n;
+
+	int a[n];
+	for(int i=0;i<n;i++){
+		std::cin >> a[i];
+	}
+
+	int maxSum = INT_MIN;
+	int currentSum = 0;
+	for(int i=0;i<n;i++){
+		currentSum += a[i];
+		maxSum = std::max(currentSum,maxSum);
+		if(currentSum < 0 ) currentSum = 0;
+	}
+	std::cout << maxSum;
+}
