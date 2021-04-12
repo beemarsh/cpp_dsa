ef765ced54157c7c9654149ad9e31c06a2c8d74e
beemarsh <anup8eguy@gmail.com>
2021-04-12 12:13:13 +0545

    Maximum Sum Subarray Code Format

--- subarray/challenges/maximum_sum_subarray/maximum_sum_subarray.cpp
+++ subarray/challenges/maximum_sum_subarray/maximum_sum_subarray.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
 #include <climits>
+#include <iostream>
 
-int main(){
-	int n;
-	std::cin >> n;
+int main() {
+  int n;
+  std::cin >> n;
 
-	int a[n];
-	for(int i=0;i<n;i++){
-		std::cin >> a[i];
-	}
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    std::cin >> a[i];
+  }
 
-	int maxSum = INT_MIN;
-	int currentSum = 0;
-	for(int i=0;i<n;i++){
-		currentSum += a[i];
-		maxSum = std::max(currentSum,maxSum);
-		if(currentSum < 0 ) currentSum = 0;
-	}
-	std::cout << maxSum;
+  int maxSum = INT_MIN;
+  int currentSum = 0;
+  for (int i = 0; i < n; i++) {
+    currentSum += a[i];
+    maxSum = std::max(currentSum, maxSum);
+    if (currentSum < 0) currentSum = 0;
+  }
+  std::cout << maxSum;
 }
