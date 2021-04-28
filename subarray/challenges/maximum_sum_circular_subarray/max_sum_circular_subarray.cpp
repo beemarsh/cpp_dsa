b4314ed1565b076d97d47d18a1ba7f0c926ceab6
beemarsh <anup8eguy@gmail.com>
2021-04-28 12:13:13 +0545

    Maximum Sum Circular Subarray code

--- subarray/challenges/maximum_sum_circular_subarray/max_sum_circular_subarray.cpp
+++ subarray/challenges/maximum_sum_circular_subarray/max_sum_circular_subarray.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <climits>
 
+int kadanes(int arr[],int n){
+	int sum = 0;
+	int maxSum = INT_MIN;
+	for(int i=0;i<n;i++){
+		sum+=arr[i];
+		if(sum < 0) sum = 0;
+		maxSum = std::max(maxSum,sum);
+	}
+	return maxSum;
+}
 int main(){
+	int n;
+	std::cin >> n;
 
+	int a[n];
+	for(int i=0;i<n;i++){
+		std::cin >> a[i];
+	}
+
+	int totalSum = 0;
+
+	for(int i=0; i<n;i++){
+		totalSum += a[i];
+		a[i] = a[i] * -1;
+	}
+
+	int max_cir_sum = totalSum + kadanes(a,n);
+
+	std::cout << max_cir_sum;
 }
