ce315d57c405e1fd0ecae6dfb1501bd4fe5a712b
beemarsh <anup8eguy@gmail.com>
2021-06-29 12:13:13 +0545

    Pair target sum code format

--- subarray/challenges/pair_target_sum/pair_target_sum.cpp
+++ subarray/challenges/pair_target_sum/pair_target_sum.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 
-int main(){
-	int n;
-	std::cin >> n;
+int main() {
+  int n;
+  std::cin >> n;
 
-	int K;
-	std::cin >> K;
+  int K;
+  std::cin >> K;
 
-	int a[n];
-	for(int i=0;i < n;i++){
-		std::cin >> a[i];
-	}
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    std::cin >> a[i];
+  }
 
-	int lp=0,rp=n-1;
+  int lp = 0, rp = n - 1;
 
-	while(lp < rp){
-		if(a[lp] + a[rp] < K) lp++;
-		else if(a[lp] + a[rp] > K) rp--;
-		else break;
-	}
+  while (lp < rp) {
+    if (a[lp] + a[rp] < K)
+      lp++;
+    else if (a[lp] + a[rp] > K)
+      rp--;
+    else
+      break;
+  }
 
-	std::cout << lp << " " << rp;
-	
+  std::cout << lp << " " << rp;
 }
