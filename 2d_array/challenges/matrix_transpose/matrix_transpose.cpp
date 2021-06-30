a8dc1c9d69b28094eafb774523726d667ca68165
beemarsh <anup8eguy@gmail.com>
2021-06-30 12:13:13 +0545

    Matrix Transpose Initialized

---
+++ 2d_array/challenges/matrix_transpose/matrix_transpose.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(){
+	int n,m;
+	std::cin >> n >> m;
+
+	int a[n][m];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			std::cin >> a[i][j];
+		}
+	}
+
+	for(int i=0;i<n;i++){
+		for(int j=i;j<m;j++){
+			int temp = a[i][j];
+			a[i][j] = a[j][i];
+			a[j][i] = temp;
+		}
+	}
+
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			std::cout << a[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
