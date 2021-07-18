428331b95392bbc597ab7154376724cf5a8d717b
beemarsh <anup8eguy@gmail.com>
2021-07-18 12:13:13 +0545

    Matrix Transpose Code updated

--- 2d_array/challenges/matrix_transpose/matrix_transpose.cpp
+++ 2d_array/challenges/matrix_transpose/matrix_transpose.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 
 int main() {
-  int n, m;
-  std::cin >> n >> m;
+  int n;
+  std::cin >> n;
 
   int a[n][m];
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
+    for (int j = 0; j < n; j++) {
       std::cin >> a[i][j];
     }
   }
 
   for (int i = 0; i < n; i++) {
-    for (int j = i; j < m; j++) {
+    for (int j = i; j < n; j++) {
       int temp = a[i][j];
       a[i][j] = a[j][i];
       a[j][i] = temp;
     }
   }
 
-  for (int i = 0; i < m; i++) {
+  for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       std::cout << a[i][j] << " ";
     }
