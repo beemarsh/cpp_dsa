ab5faa3a03e2742e3705680e1287591e8ec774ed
beemarsh <anup8eguy@gmail.com>
2021-08-01 12:13:13 +0545

    Matrix Multiplication Code Layout

---
+++ 2d_array/challenges/matrix_multiplication/matrix_multiplication.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int n;
+  std::cin >> n;
+
+  int a[n][m];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      std::cin >> a[i][j];
+    }
+  }
+
+}
