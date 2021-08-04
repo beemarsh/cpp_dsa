4ecdfe15531da5cf658a491c9378ad149082bb8f
beemarsh <anup8eguy@gmail.com>
2021-08-04 12:13:13 +0545

    Matrix Multiplication code

--- 2d_array/challenges/matrix_multiplication/matrix_multiplication.cpp
+++ 2d_array/challenges/matrix_multiplication/matrix_multiplication.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
 
 int main() {
-  int n;
-  std::cin >> n;
+  int n1,m1,n2,m2;
+  std::cin >> n1 >> m1;
+  std::cin >> n2 >> m2;
 
-  int a[n][m];
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      std::cin >> a[i][j];
+  if(m1 != n2) {
+  	std::cout << "Matrix Multiplication Not Possible";
+	return -1;
+	}
+
+
+  int a1[n1][m1];
+  for (int i = 0; i < n1; i++) {
+    for (int j = 0; j < m1; j++) {
+      std::cin >> a1[i][j];
     }
   }
 
+
+  int a2[n2][m2];
+  for (int i = 0; i < n2; i++) {
+    for (int j = 0; j < m2; j++) {
+      std::cin >> a2[i][j];
+    }
+	}
+
+	int A[n1][m2];
+
+	for(int i=0;i<n1;i++){
+		for(int j=0;j<m2;j++){
+			int sum = 0;
+			for(int k=0;k<m1;k++){
+				sum+= a1[i][k] * a2[k][j];
+			}
+			A[i][j] = sum;
+		}
+	}
+
+	for(int i=0;i<n1;i++){
+		for(int j=0;j<m2;j++){
+			std::cout << A[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
 }
